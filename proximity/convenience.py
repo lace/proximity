7b645e0be7669b2eda7e3bb42a0d9b2a30717072
Paul Melnikow <email@paulmelnikow.com>
2020-05-13 15:26:42 -0400

    Document missing params, add validation

--- proximity/convenience.py
+++ proximity/convenience.py
@@ -9,6 +9,8 @@ def faces_nearest_to_points(vertices, faces, query_points, ret_points=False):
     points.
 
     Args:
+        vertices (np.arraylike): The vertices of a mesh, as `kx3`.
+        faces (np.arraylike): The face indices of a mesh, as `kx3`.
         query_points (np.arraylike): The points to query, with shape `kx3`
         ret_points (bool): When `True`, return both the indices of the
             nearest faces and the closest points to the query points, which
@@ -16,12 +18,13 @@ def faces_nearest_to_points(vertices, faces, query_points, ret_points=False):
             face indices.
 
     Returns:
-        object: face indices as `kx1 np.ndarray`, or when `ret_points`
+        object: Face indices as `kx1 np.ndarray`, or when `ret_points`
         is `True`, a tuple also including the coordinates of the closest points
         as `kx3 np.ndarray`.
     """
     vg.shape.check(locals(), "vertices", (-1, 3))
     vg.shape.check(locals(), "faces", (-1, 3))
+    vg.shape.check(locals(), "query_points", (-1, 3))
 
     trimesh = MockTrimesh(vertices=vertices, faces=faces)
     closest_points, _, face_indices = closest_point(trimesh, query_points)
