9e0d115e9f7fb67af78ed3e73cfd8cd6564ef906
Paul Melnikow <email@paulmelnikow.com>
2026-02-26 16:54:44 -0500

    Dev.py types

--- dev.py
+++ dev.py
@@ -5,7 +5,7 @@ import click
 from executor import execute
 
 
-def python_source_files():
+def python_source_files() -> list[str]:
     import glob
 
     include_paths = (
@@ -19,71 +19,71 @@ def python_source_files():
 
 
 @click.group()
-def cli():
+def cli() -> None:
     pass
 
 
 @cli.command()
-def install():
+def install() -> None:
     execute("poetry install --sync")
 
 
 @cli.command()
-def test():
+def test() -> None:
     execute("pytest")
 
 
 @cli.command()
-def coverage():
+def coverage() -> None:
     execute("pytest --cov=proximity")
 
 
 @cli.command()
-def coverage_report():
+def coverage_report() -> None:
     execute("coverage html")
     execute("open htmlcov/index.html")
 
 
 @cli.command()
-def check_types():
+def check_types() -> None:
     execute("mypy --show-error-codes proximity/")
     execute("mypy --show-error-codes dev.py")
 
 
 @cli.command()
-def lint():
+def lint() -> None:
     execute("flake8", *python_source_files())
 
 
 @cli.command()
-def black():
+def black() -> None:
     execute("black", *python_source_files())
 
 
 @cli.command()
-def black_check():
+def black_check() -> None:
     execute("black", "--check", *python_source_files())
 
 
 @cli.command()
-def doc():
+def doc() -> None:
     execute("rm -rf build/ doc/build/ doc/api/")
     execute("sphinx-build -W -b singlehtml doc doc/build")
 
 
 @cli.command()
-def doc_open():
+def doc_open() -> None:
     execute("open doc/build/index.html")
 
 
 @cli.command()
-def clean():
+def clean() -> None:
     execute("find . -name '*.pyc' -delete")
     execute("find . -name '__pycache__' -delete")
 
 
 @cli.command()
-def publish():
+def publish() -> None:
     execute("rm -rf dist/ build/")
     execute("poetry build")
     execute("twine upload dist/*")
